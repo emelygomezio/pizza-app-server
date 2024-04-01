593eba98679e7b97821b7d1a3648448e025bde1d
John Doe <johndoe@example.com>
2024-04-01 15:04:28 -0400

    Updated middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,54 @@
 import { auth } from "express-oauth2-jwt-bearer";
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+declare global {
+    namespace Express {
+        interface Request {
+            userId: string;
+            auth0Id: string;
+        }
+    }
+}
 
 export const jwtCheck = auth ({
-    audience: 'pizza-app-api',
-    issuerBaseURL: 'https://dev-13z36bkbwb8banhr.us.auth0.com/',
+    audience: process.env.AUTH0_AUDIENCE,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
     tokenSigningAlg: 'RS256'
   });
+
+  export const jwtParse = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+
+    const { authorization } = req.headers;
+
+    if(!authorization || !authorization.startsWith("Bearer")) {
+        return res.sendStatus(401);
+    }
+
+    const token = authorization.split(" ")[1];
+
+    try {
+        const decoded = jwt.decode(token) as jwt.JwtPayload;
+        const auth0Id = decoded.sub;
+
+        const user = await User.findOne({ auth0Id })
+
+        if (!user) {
+            return res.sendStatus(401);
+        }
+        
+        req.auth0Id = auth0Id as string;
+        req.userId = user._id.toString();
+        next();
+
+    } catch (error) {
+        return res.sendStatus(401);
+    }
+ 
+
+  };
