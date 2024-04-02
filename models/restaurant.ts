dc5b9188efa0e7adf6372258fa4f932e25870b94
John Doe <johndoe@example.com>
2024-04-02 14:16:53 -0400

    Fixed findOne bug

--- models/restaurant.ts
+++ models/restaurant.ts
@@ -5,6 +5,19 @@ const menuItemSchema = new mongoose.Schema({
     price: { type: Number, required: true },
 });
 
+// const restaurantSchema = new mongoose.Schema({
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+//     restaurantName: { type: String, required: true },
+//     city: { type: String, required: true },
+//     country: { type: String, required: true },
+//     deliveryPrice: { type: Number, required: true },
+//     estimatedDeliveryTime: { type: Number, required: true },
+//     cuisines: [{ types: String, required: true }],
+//     menuItems: [menuItemSchema],
+//     imageUrl: { type: String, required: true },
+//     lastUpdated: { type: Date, required: true },
+// });
+
 const restaurantSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     restaurantName: { type: String, required: true },
@@ -12,11 +25,11 @@ const restaurantSchema = new mongoose.Schema({
     country: { type: String, required: true },
     deliveryPrice: { type: Number, required: true },
     estimatedDeliveryTime: { type: Number, required: true },
-    cuisines: [{ types: String, required: true }],
+    cuisines: [{ type: String, required: true }],
     menuItems: [menuItemSchema],
     imageUrl: { type: String, required: true },
     lastUpdated: { type: Date, required: true },
-});
+  });
 
 const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 export default Restaurant; 
