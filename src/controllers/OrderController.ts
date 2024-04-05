cc6940ad872f3090853b0a3f23baf558cefc8dd0
John Doe <johndoe@example.com>
2024-04-05 00:40:14 -0400

    Added Order Controller functionality

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
 import { Request, Response } from "express";
 import Restaurant, { MenuItemType } from "../../models/restaurant";
+import Order from "../../models/order";
 
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
@@ -33,6 +34,15 @@ const createCheckoutSession = async (req: Request, res: Response) => {
         throw new Error("Restaurant not found");
     }
 
+    const newOrder = new Order ({
+        restaurant: restaurant,
+        user: req.userId, 
+        status: "placed",
+        deliveryDetails: checkoutSessionRequest.deliveryDetails,
+        cartItems: checkoutSessionRequest.cartItems,
+        createdAt: new Date(),
+    });
+
     const lineItems = createLineItems(
         checkoutSessionRequest,
         restaurant.menuItems
@@ -49,8 +59,10 @@ const createCheckoutSession = async (req: Request, res: Response) => {
         return res.status(500).json({ message: "Error creating Stripe session" });
       }
 
+      await newOrder.save();
+
       res.json({ url: session.url });
-      
+
   } catch (error: any) {
     console.log(error);
     res.status(500).json({ message: error.raw.message });
